f6b4e76ac42160c9e72e19e69bb343f51c2faaeb
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-10-06 23:58:40 +0900

    Boj_1296 add

---
+++ src/text/bronze/BOJ_1296.java
@@ -0,0 +1,19 @@
+package text.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+
+/*아직*/
+public class BOJ_1296 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String n = br.readLine();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++){
+
+        }
+    }
+
+}
