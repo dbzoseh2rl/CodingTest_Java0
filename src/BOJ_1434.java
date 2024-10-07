2bb949d8eafed7155f011b4ffb5999c919be0350
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-10-07 15:24:52 +0900

    Boj_1434 add

---
+++ src/BOJ_1434.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.System.*;
+
+public class BOJ_1434 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = br.readLine();
+        out.println("A = " + A);
+        String B = br.readLine();
+        out.println("B = " + B);
+        String C = br.readLine();
+        out.println("C = " + C);
+    }
+}
