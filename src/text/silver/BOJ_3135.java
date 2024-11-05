8a8dc6da5586353ec0f2d6598667ed51fa733007
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-11-05 09:27:36 +0900

    refactor: BOJ_3135 soltion2 add.

--- src/text/silver/BOJ_3135.java
+++ src/text/silver/BOJ_3135.java
@@ -3,9 +3,9 @@ package text.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// solution 1
 public class BOJ_3135 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,13 +14,37 @@ public class BOJ_3135 {
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(br.readLine());
-        int p = 0;
         int min = Math.abs(A - B);
         for (int i = 0; i < N; i++) {
             int k = Integer.parseInt(br.readLine());
             min = Math.min(Math.abs(B - k) + 1, min);
         }
-        System.out.println(min + p);
-
+        System.out.println(min);
     }
 }
+
+/*
+// solution 2
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_3135 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstLine = br.readLine().split(" ");
+        int A = Integer.parseInt(firstLine[0]);
+        int B = Integer.parseInt(firstLine[1]);
+        int N = Integer.parseInt(br.readLine());
+
+        int min = Math.abs(A - B);
+        for (int i = 0; i < N; i++) {
+            int k = Integer.parseInt(br.readLine());
+            min = Math.min(Math.abs(B - k) + 1, min);
+        }
+
+        System.out.println(min);
+    }
+}
+*/
