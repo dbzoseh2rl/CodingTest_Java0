089d9bc3788b1e3a552c89f6d6ec85bed998be39
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-11-12 14:49:03 +0900

    fix: swea_2071

--- src/text/silver/BOJ_7859.java
+++ src/text/silver/BOJ_7859.java
@@ -1,5 +1,3 @@
-package text.silver;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,45 +5,51 @@ import java.util.StringTokenizer;
 
 public class BOJ_7859 {
     static int N;
-    static int maxTraffic = 0;
+    static int[][] map;
+    static int max = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int TC = Integer.parseInt(br.readLine());
-        int[][] map = new int[TC][TC];
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
 
-        for (int i = 0; i < TC; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < TC; j++) {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        // 여기까진 맞는데...
 
-        int result = 0;
-
-        for (int y = 0; y < TC; y++) {
-            int sum = 0;
-            for (int x = 0; x < TC; x++) {
-                sum += map[y][x];
-            }
-            result= Math.max(result, sum);
-        }
-
-        System.out.println(result);
+        dfs(0, new int[N]);
+        System.out.println(max);
     }
-    static void dfs(int idx, boolean[] selected) {
-        if (idx == N) { // 노드 분할
-            maxTraffic = Math.max(maxTraffic, );
+
+    static void dfs(int idx, int[] selected) {
+        if (idx == N) {
+            /*for (int i = 0; i < N; i++) {
+                System.out.print(selected[i] + " ");  // 0 또는 1로 출력
+            }
+            System.out.println();*/
+            int Sum = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    if ((selected[i] == 0 && selected[j] == 1) || (selected[i] == 1 && selected[j] == 0)) {
+                        Sum += map[i][j];
+                    }
+                }
+            }
+            if (Sum > max) {
+                max = Sum;
+            }
             return;
         }
 
-        // 노드가 A로 나뉠떄
-        selected[idx] = true;
+        // 노드를 그룹 A(0)에 포함시키는 경우
+        selected[idx] = 0;
         dfs(idx + 1, selected);
 
-        // 노드가 B로 나뉠때
-        selected[idx] = false;
+        // 노드를 그룹 B(1)에 포함시키는 경우
+        selected[idx] = 1;
         dfs(idx + 1, selected);
     }
 }
