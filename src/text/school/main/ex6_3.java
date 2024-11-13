9e5ff7f25766ec078f38fea924c53f923d30e03e
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-11-13 18:35:40 +0900

    feat: ex6_3 add

---
+++ src/text/school/main/ex6_3.java
@@ -0,0 +1,10 @@
+package text.school.main;
+
+import text.school.util.Calc;
+
+public class ex6_3 {
+    public static void main(String[] args) {
+        Calc c = new Calc(10, 20);
+        System.out.println(c.sum());
+    }
+}
