d1283aebcf4a53232ad53b2ae387e346d0474334
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-11-29 18:28:40 +0900

    feat: programmus_68644 add

--- src/text/deepdive_study/Programmus_괄호회전하기.java
+++ src/text/deepdive_study/Programmus_괄호회전하기.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Programmus_올바른괄호 {
+public class Programmus_괄호회전하기 {
     static class Solution {
         public boolean solution(String s) {
             int cnt = 0;
