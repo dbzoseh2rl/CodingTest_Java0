6427736319d17e25f6f7426d3d05affbbc252e81
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-12-07 19:48:31 +0900

    feat: BOJ_1347 add

---
+++ src/text/silver/BOJ_1347.java
@@ -0,0 +1,15 @@
+package text.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1347 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+    }
+}
