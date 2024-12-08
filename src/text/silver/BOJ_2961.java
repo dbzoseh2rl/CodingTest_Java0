fa810258fd83e1858f436a1e2455dc3da5beb0cc
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-12-08 21:31:04 +0900

    feat: BOJ_2961 add

---
+++ src/text/silver/BOJ_2961.java
@@ -0,0 +1,12 @@
+package text.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2961 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
