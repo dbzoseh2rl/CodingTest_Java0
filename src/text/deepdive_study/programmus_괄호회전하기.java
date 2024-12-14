17a7f8d49aac4376beae272e586c1a8cb094cc2f
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-12-14 22:43:00 +0900

    feat: ex8_1.java add

--- src/text/deepdive_study/programmus_괄호회전하기.java
+++ src/text/deepdive_study/programmus_괄호회전하기.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Programmus_괄호회전하기 {
+public class programmus_괄호회전하기 {
     static class Solution {
         public boolean solution(String s) {
             int cnt = 0;
