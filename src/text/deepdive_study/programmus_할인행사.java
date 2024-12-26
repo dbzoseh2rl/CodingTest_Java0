50f18a3862d0b08c3a15a854c939c757da033768
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-12-26 10:00:00 +0900

    feat: swea_1240 add

---
+++ src/text/deepdive_study/programmus_할인행사.java
@@ -0,0 +1,19 @@
+package text.deepdive_study;
+
+public class programmus_할인행사 {
+    public static void main(String[] args) {
+        String[] want1 = {"banana", "apple", "rice", "pork", "pot"};
+        int[] number1 = {3, 2, 2, 2, 1};
+        String[] discount1 = {"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"};
+
+        System.out.println(solution(want1, number1, discount1));
+    }
+
+    public static int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+}
