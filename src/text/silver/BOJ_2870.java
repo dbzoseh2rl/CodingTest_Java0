3e6dc32d7552e0423a60fba8450f11811f37d21f
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-12-27 19:26:30 +0900

    feat: BOJ_2870 add

---
+++ src/text/silver/BOJ_2870.java
@@ -0,0 +1,33 @@
+/*
+package text.silver;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BOJ_2870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] map = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        ArrayList<String> input_list = new ArrayList<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String a = br.readLine();
+            input_list.add(a);
+        }
+
+        StringBuilder concated_list = new StringBuilder();
+        for (String str : input_list) {
+            concated_list.append(str);
+        }
+
+        System.out.println(concated_list.toString());
+
+
+    }
+}
+*/
