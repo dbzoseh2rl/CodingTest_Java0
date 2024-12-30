afbe84c1ffdb9b0ac89605d65dc5e11f836b0b30
dbzoseh2rl <dbzoseh2rl123@naver.com>
2024-12-30 17:50:08 +0900

    feat: swea_1240 add

---
+++ src/text/swea/D3/swea_1240.java
@@ -0,0 +1,22 @@
+/*
+package text.swea.D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class swea_1240 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+
+        }
+
+
+    }
+}
+*/
