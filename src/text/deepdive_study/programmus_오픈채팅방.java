70f27d6d62dd58713ee75d26748ed870cacfa7af
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-01-03 23:45:22 +0900

    feat: programmus_오픈채팅방 add

--- src/text/deepdive_study/programmus_오픈채팅방.java
+++ src/text/deepdive_study/programmus_오픈채팅방.java
@@ -2,6 +2,7 @@ package text.deepdive_study;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class programmus_오픈채팅방 {
@@ -14,15 +15,37 @@ public class programmus_오픈채팅방 {
 
     public static String[] solution(String[] record) {
         String[] answer = {};
+        List<String> answerList = new ArrayList<>();
 
-        for(String e : record){
-            String[] str = e.split(" ");
-            System.out.println(Arrays.toString(str));
+//        for(String e : record){
+//            String[] str = e.split(" ");
+//            System.out.println(Arrays.toString(str));
+//        }
+        HashMap<String, String> hm = new HashMap<>();
+
+        for(int i = 0; i<record.length; i++){
+            String[] strArr = record[i].split(" ");
+
+            if(strArr[0].equals("Enter")){
+                hm.put(strArr[1],strArr[2]);
+            }
+            else if(strArr[0].equals("Change")){
+                hm.put(strArr[1],strArr[2]);
+            }
         }
 
+        for(int i =0; i<record.length; i++){
+            String[] strArr = record[i].split(" ");
 
+            if(strArr[0].equals("Enter")){
+                answerList.add(hm.get(strArr[1])+"님이 들어왔습니다.");
+            }
+            else if(strArr[0].equals("Leave")){
+                answerList.add(hm.get(strArr[1])+"님이 나갔습니다.");
+            }
+        }
 
-
+        answer = answerList.stream().toArray(String[]::new);
         return answer;
     }
 }
