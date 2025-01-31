dfaec2c71e1fcae1d79e4054dd1ba37ae0fa3151
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-01-31 21:45:01 +0900

    feat: BOJ_32326 add

---
+++ src/text/bronze/BOJ_32326.java
@@ -0,0 +1,17 @@
+package text.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_32326 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+        int g = sc.nextInt();
+        int b = sc.nextInt();
+        int c = r * 3 + g * 4 + b * 5;
+
+        System.out.println(c);
+        sc.close();
+    }
+}
