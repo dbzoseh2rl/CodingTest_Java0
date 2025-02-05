5bc98eb7f0b1518c08d6a3a37bc863e8e8d97644
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-02-05 23:25:37 +0900

    feat: BOJ_33162 add

---
+++ src/text/bronze/BOJ_33162.java
@@ -0,0 +1,14 @@
+package text.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_33162 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println(a / 5);
+        sc.close();
+    }
+}
