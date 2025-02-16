ea632dae1b0f19b9e149620aa33125a7e7ba4b2b
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-02-16 23:35:29 +0900

    feat: BOJ_31922 add

---
+++ src/text/bronze/BOJ_31922.java
@@ -0,0 +1,21 @@
+package text.bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_31922 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken()); // Division 1 우승 상금
+        int p = Integer.parseInt(st.nextToken()); // Division 2 우승 상금
+        int c = Integer.parseInt(st.nextToken()); // shake! 우승 상금
+        int sum = a + c > p ? a + c : p;
+
+        bw.write(Integer.toString(sum));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
