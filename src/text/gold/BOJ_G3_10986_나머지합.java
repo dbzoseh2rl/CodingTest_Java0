7b729019fc36c6eb0f66d74f278693a02efbc164
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-02 16:08:28 +0900

    feat: BOJ_10986 add

---
+++ src/text/gold/BOJ_G3_10986_나머지합.java
@@ -0,0 +1,41 @@
+package text.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_G3_10986_나머지합 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        long[] sum = new long[n];
+        int[] mode = new int[m];
+        st = new StringTokenizer(br.readLine());
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        sum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int modeNum = (int) (sum[i] % m);
+            if (modeNum == 0) ans++;
+            mode[modeNum]++;
+        }
+
+        for (int i = 0; i < m; i++) {
+            ans += (long) mode[i] * (mode[i] - 1) / 2;
+        }
+
+        System.out.println(ans);
+    }
+
+}
