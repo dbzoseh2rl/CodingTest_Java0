33389be692c8cb77c6f8701b3445f7f681c4e796
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-05 22:32:55 +0900

    fix: BOJ_B3_10824

--- src/text/bronze/BOJ_B3_10824.java
+++ src/text/bronze/BOJ_B3_10824.java
@@ -9,12 +9,13 @@ public class BOJ_B3_10824 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         String a = st.nextToken();
         String b = st.nextToken();
+        long first = Long.parseLong(a+b);
         String c = st.nextToken();
         String d = st.nextToken();
+        long second = Long.parseLong(c+d);
+        System.out.print(first + second);
 
-        System.out.println(Integer.parseInt(a + b) + Integer.parseInt(c + d));
     }
 }
