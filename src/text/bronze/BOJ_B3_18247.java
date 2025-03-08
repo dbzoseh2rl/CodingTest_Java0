92d38f9fa725354d861476e96c034af0f7d9e2ca
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-03-08 22:52:38 +0900

    fix: BOJ_B3_18247

--- src/text/bronze/BOJ_B3_18247.java
+++ src/text/bronze/BOJ_B3_18247.java
@@ -1,33 +1,33 @@
 package text.bronze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BOJ_B3_18247 {
-    public static void main(String[] args) {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner scan = new Scanner(System.in);
-        int L = 12;
+    public static void main(String[] args) throws IOException {
 
-        int Tcase = scan.nextInt();
+        int testCase = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < Tcase; i++) {
-            int N = scan.nextInt();
-            //행
-            int M = scan.nextInt();
-            //열
-            if(N < 12 || M < 4) {
-                System.out.println(-1);
-                break;
-                //N이 12보다 작고 M이 4보다 작으면
-                //성립할 수 없다.
-            }else {
-                int answer = (L * M) - (M - 4);
-                //L 까지의 전체 자석에서  (M-4) 를 뺴주면 L4의 위치가 된다.
-                System.out.println(answer);
+        for (int i = 0; i < testCase; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+
+            int position = -1;
+
+            // Alphabetical position of L : 12
+            if (y >= 12 && x >= 4) {
+                position = 11 * x + 4;
             }
 
-        }
+            System.out.println(position);
 
+        }
     }
 }
 
