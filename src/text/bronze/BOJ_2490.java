82f9e3ed85476ecc166b4b8eb1474c1e8e1d99d2
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-11 23:00:00 +2300

    feat: BOJ_B3_ 2490 add

---
+++ src/text/bronze/BOJ_2490.java
@@ -0,0 +1,37 @@
+package text.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2490 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int sum = a + b + c + d;
+
+            if (sum == 3) {
+                System.out.println("A");
+            }
+            else if (sum == 2) {
+                System.out.println("B");
+            }
+            else if (sum == 1) {
+                System.out.println("C");
+            }
+            else if (sum == 0) {
+                System.out.println("D");
+            }
+            else
+                System.out.println("E");
+        }
+    }
+}
