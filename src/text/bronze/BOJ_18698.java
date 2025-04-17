b67907499cb0786cb2375b62238ddc0181e3e248
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-17 21:32:46 +0900

    feat: BOJ_B4_18698 add

---
+++ src/text/bronze/BOJ_18698.java
@@ -0,0 +1,24 @@
+package text.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_18698 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for(int i = 0; i < t; i++) {
+            String a = sc.next();
+            int count = 0;
+            for(int j = 0; j < a.length(); j++) {
+                if(a.charAt(j) == 'D') {
+                    break;
+                }
+                count++;
+            }
+            System.out.println(count);
+        }
+        sc.close();
+    }
+}
