ec6be2c41eeaf0883dabafc65aac02258335bf48
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-19 23:00:00 +2300

    feat: BOJ_B4_17874 add

---
+++ src/text/bronze/BOJ_17874.java
@@ -0,0 +1,20 @@
+package text.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_17874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int res = Math.max(h, n - h) * Math.max(v, n - v);
+
+        System.out.println(res * 4);
+    }
+}
