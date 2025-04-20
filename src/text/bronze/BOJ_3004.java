5697ea0fd60d1535518c1494cf49ed4f47118e8f
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-20 19:02:40 +0900

    feat: BOJ_B3_3004 add

---
+++ src/text/bronze/BOJ_3004.java
@@ -0,0 +1,22 @@
+package text.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_3004 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for(int i = 1; i <= N; i++) {
+            if(i == 1) {
+                sum += 2;
+            }else {
+                sum += ((i / 2) + 1);
+            }
+        }
+        System.out.println(sum);
+    }
+}
