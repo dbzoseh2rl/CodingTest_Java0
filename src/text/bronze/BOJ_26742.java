4319cce19be580983ddb782b5a1fc52087642a1e
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-25 21:29:36 +0900

    feat: BOJ_B4_26742 add

---
+++ src/text/bronze/BOJ_26742.java
@@ -0,0 +1,27 @@
+package text.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_26742 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] socks = br.readLine().toCharArray();
+
+        int whiteCount = 0;
+        int blackCount = 0;
+
+        for (char c : socks) {
+            if (c == 'B') {
+                whiteCount++;
+            } else {
+                blackCount++;
+            }
+        }
+
+        int pair = whiteCount / 2 + blackCount / 2;
+        System.out.println(pair);
+    }
+}
