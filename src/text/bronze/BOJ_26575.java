2bb845a3f2f1b923e40aed70fea77f97c17e32c8
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-26 18:47:16 +0900

    feat: BOJ_B4_26575 add

---
+++ src/text/bronze/BOJ_26575.java
@@ -0,0 +1,19 @@
+package text.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_26575 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            double d=sc.nextDouble();
+            double f=sc.nextDouble();
+            double p=sc.nextDouble();
+
+            String s=String.format("%.2f",d*f*p);
+            System.out.print("$");
+            System.out.println(s);
+        }
+    }
+}
