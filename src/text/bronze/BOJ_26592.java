8dadfd6b42bc8ca5f3a6328aa2914b7051916c62
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-04-28 23:18:11 +0900

    feat: BOJ_B4_26592 add

---
+++ src/text/bronze/BOJ_26592.java
@@ -0,0 +1,19 @@
+package text.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_26592 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double h = (2 * a) / b;
+            System.out.println("The height of the triangle is " + String.format("%.2f", h) + " units");
+        }
+        sc.close();
+    }
+}
