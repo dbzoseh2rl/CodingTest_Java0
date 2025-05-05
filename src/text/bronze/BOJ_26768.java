33bb4a02eb49a087b058a55afb42b79955e0ac83
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-05-05 17:00:42 +0900

    feat: BOJ_B4_26768 add

---
+++ src/text/bronze/BOJ_26768.java
@@ -0,0 +1,19 @@
+package text.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_26768 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+
+        s = s.replace("a", "4").replace("e", "3").replace("i", "1").replace("o", "0").replace("s", "5");
+
+        System.out.println(s);
+    }
+
+}
