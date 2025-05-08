7fd10ec86904d0c543eac9aafff857ae77daef34
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-05-08 23:00:00 +2300

    feat: BOJ_B4_23795 add

---
+++ src/text/bronze/BOJ_23795.java
@@ -0,0 +1,21 @@
+package text.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_23795 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        while (true) {
+            int cur = Integer.parseInt(br.readLine());
+            if (cur == -1) break;
+            sum+=cur;
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
