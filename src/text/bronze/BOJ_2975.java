787c59e6f3d005ecd30cdf9cdb77f957f75fe72e
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-05-10 23:54:39 +0900

    feat: BOJ_B3_2975 add

---
+++ src/text/bronze/BOJ_2975.java
@@ -0,0 +1,35 @@
+package text.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_2975 {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = 0;
+        String trans = "";
+        int b = 0;
+
+        while (true) {
+            a = sc.nextInt();
+            trans = sc.next();
+            b = sc.nextInt();
+
+            if (a == 0 && trans.equals("W") && b == 0) break;
+
+            if (trans.equals("D")) {
+                System.out.println(a + b);
+            }
+            else {
+                if (a - b < -200) {
+                    System.out.println("Not allowed");
+                }
+                else {
+                    System.out.println(a - b);
+                }
+            }
+        }
+
+    }
+}
