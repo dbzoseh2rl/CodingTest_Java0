d3dac15de9baf92bf08389949fadb77dca207b24
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-05-21 22:08:27 +0900

    feat: BOJ_B3_31789 add

--- src/text/bronze/BOJ_2783.java
+++ src/text/bronze/BOJ_2783.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ2783 {
+public class BOJ_2783 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
