35fca0c92bfa9bf7fcc14153324c5c9043a8b137
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-06-05 00:15:32 +0900

    feat: BOJ_13223  add

--- src/text/bronze/BOJ_13223.java
+++ src/text/bronze/BOJ_13223.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_13223 {
+public class
+BOJ_13223 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
