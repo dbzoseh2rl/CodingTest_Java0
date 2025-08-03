de7765944e51aa6ae553db7e315c712fd1d275b9
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-08-03 20:44:03 +0900

    feat: BOJ_3049 add

---
+++ src/text/bronze/BOJ_3049.java
@@ -0,0 +1,19 @@
+package text.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_3049 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int result = n * (n - 1) * (n - 2) * (n - 3) / 24;
+
+        System.out.println(result);
+
+    }
+}
