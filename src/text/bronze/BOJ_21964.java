2251f16383b9319b953fe38893a4747e8dfbac6d
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-08-09 18:41:43 +0900

    feat: BOJ_21964 add

---
+++ src/text/bronze/BOJ_21964.java
@@ -0,0 +1,18 @@
+package text.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_21964 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String a = sc.next();
+
+        for (int i = a.length() - 5; i < a.length(); i++) {
+            System.out.print(a.charAt(i));
+        }
+        sc.close();
+    }
+}
