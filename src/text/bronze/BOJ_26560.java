a1a9f90751f46ad6ddb1b05894c816b467fffd3f
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-08-13 10:00:00 +0900

    feat: BOJ_26580 ADD

---
+++ src/text/bronze/BOJ_26560.java
@@ -0,0 +1,20 @@
+package text.bronze;
+
+import java.util.Scanner;
+
+public class BOJ_26560 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int total = a + (b / 4) - (b / 7);
+            System.out.println(total);
+        }
+        sc.close();
+    }
+}
