22d324583a494109378bcad967be76a1547b9cfc
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-08-25 22:23:12 +0900

    feat: BOJ_24075 ADD

--- src/text/bronze/BOJ_24075.java
+++ src/text/bronze/BOJ_24075.java
@@ -1,21 +1,22 @@
 package text.bronze;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class BOJ_24075 {
-    public static void main(String[] args) {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = a+b;
+        int d = a-b;
+        System.out.printf("%d %d", c>d?c:d, c>d?d:c);
+    }
 
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-
-        if(a <= c && c < b) {
-            System.out.println("1");
-        }else {
-            System.out.println("0");
-        }
-        sc.close();
+    public static void main(String[] args) throws Exception {
+        new BOJ_24075().solution();
     }
 }
