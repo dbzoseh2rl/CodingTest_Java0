38528b0a20a53da5c50677d7b958ef385cb58ed0
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-09-26 15:22:26 +0900

    feat: BOJ_4335 ADD

--- src/text/bronze/BOJ_17618.java
+++ src/text/bronze/BOJ_17618.java
@@ -35,4 +35,5 @@ public class BOJ_17618 {
             }
             System.out.println(total);
         }
+    }
 }
---
+++ src/text/silver/BOJ_4335.java
@@ -0,0 +1,39 @@
+package text.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_4335 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int low = 1;
+        int high = 10;
+        int a;
+
+        while (true) {
+            a = Integer.parseInt(br.readLine());
+            if (a == 0) break;
+
+            String b = br.readLine();
+
+            if (b.equals("too high")) {
+                high = Math.min(high, a - 1);
+            } else if (b.equals("too low")) {
+                low = Math.max(low, a + 1);
+            } else if (b.equals("right on")) {
+                // 정답 판정
+                if (a >= low && a <= high) {
+                    System.out.println("Stan is honest");
+                } else {
+                    System.out.println("Stan is dishonest");
+                }
+
+                low = 1;
+                high = 10;
+            }
+        }
+    }
+
+}
