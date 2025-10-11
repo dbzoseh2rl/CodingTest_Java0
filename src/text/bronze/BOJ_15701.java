c701c856351c379519019e60c74ac3e638f904a9
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-10-11 16:53:59 +0900

    feat: BOJ_15701 ADD

--- src/text/bronze/BOJ_15701.java
+++ src/text/bronze/BOJ_15701.java
@@ -1,24 +1,22 @@
 package text.bronze;
+
 import java.util.Scanner;
 
 public class BOJ_15701 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        sc.close();
 
-    public class Main {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            long N = sc.nextLong();
-            sc.close();
+        int count = 0;
 
-            int count = 0;
-
-            for (long i = 1; i * i <= N; i++) {
-                if (N % i == 0) {
-                    if (i * i == N) count += 1;   // a == b 인 경우 (한 쌍만)
-                    else count += 2;              // (a, b), (b, a)
-                }
+        for (long i = 1; i * i <= N; i++) {
+            if (N % i == 0) {
+                if (i * i == N) count += 1;   // a == b 인 경우 (한 쌍만)
+                else count += 2;              // (a, b), (b, a)
             }
-
-            System.out.println(count);
         }
+
+        System.out.println(count);
     }
 }
